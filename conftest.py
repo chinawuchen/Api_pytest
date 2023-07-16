809803707d6da34b8cd0ae76d68207720a24315f
chinawuchen <1102055693@qq.com>
2023-07-16 14:46:07 +0800

    conf

--- conftest.py
+++ conftest.py
@@ -2,15 +2,14 @@ import pytest
 import sys
 import os
 
-base_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+base_path = os.path.abspath("")
 sys.path.append(base_path)
 
 
+log_file = base_path + "/Log/log.txt"
 # 每次执行用例之前清空一次日志
 @pytest.fixture(scope="class")
 def truncate_log():
-    log_file = base_path + "/Api_pytest/Log/log.txt"
     with open(log_file, 'w') as f:
         f.seek(0, 0)  # 把文件定位到0;truncate也是从这里开始删除
         f.truncate()
-
